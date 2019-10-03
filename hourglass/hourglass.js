933d366e7f185588292fe4a5a65c9575ba9e74b0
Peter Tran <tran.peter08@gmail.com>
2019-10-02 21:38:16 -0700

    removed space

--- hourglass/hourglass.js
+++ hourglass/hourglass.js
@@ -5,15 +5,14 @@ function hourglass(arr) {
   const results = [];
   for (let i = 0; i< movesY; i++) {
       for (let j = 0; j < movesX; j++) {
-          let top = arr[i].slice(0 + j, 3 + j);
-          let mid = arr[i + 1][1 + j];
-          let btm = arr[i + 2].slice(0 + j, 3 + j);
+        let top = arr[i].slice(0 + j, 3 + j);
+        let mid = arr[i + 1][1 + j];
+        let btm = arr[i + 2].slice(0 + j, 3 + j);
 
-          const nums = [...top, mid, ...btm];
-          results.push(nums.reduce((total, item) => total + item, 0));
+        const nums = [...top, mid, ...btm];
+        results.push(nums.reduce((total, item) => total + item, 0));
       }
   }
-
 
   return (Math.max(...results));
 }
