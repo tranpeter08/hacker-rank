a7c67b877e0e4607a466775064fd2bd12fe74a60
Peter Tran <tran.peter08@gmail.com>
2019-10-03 02:48:47 -0700

    reverted to original solution

--- min-bribes/min-bribes.js
+++ min-bribes/min-bribes.js
@@ -1,54 +1,36 @@
 function minBribes(q) {
-  let bribes = 0;
+  const bribes = {};
+  let sorting = true;
+  while (sorting) {
+    sorting = false;
+    for (let i = 0; i < q.length - 1; i++) {
+      if (q[i] > q[i + 1]) {
+        sorting = true;
+        if (!bribes[q[i]]) {
+          bribes[q[i]] = 1;
+        } else {
+          bribes[q[i]]++;
+        }
 
-  for (let i = 0; i < q.length ; i++) {
-    if (q[i] > q[i + 1] && q[i] > q[i + 2] && q[i] > q[i + 3]) {
-      return console.log('Too chaotic');
+        [q[i], q[i + 1]] = [q[i + 1], q[i]];
+      }
     }
+  }
 
-    if (q[i] > i + 1) {
-      bribes++;
+  let total = 0;
+  let isChaos = false;
+  for (let [key, value] of Object.entries(bribes)) {
+    total+= value;
+    if (value > 2) {
+      isChaos = true;
     }
+  }
 
-    if (q[i] > i + 2) {
-      bribes++;
-    }
-  };
-
-  console.log(bribes);
-
-  // const bribes = {};
-  // let sorting = true;
-  // while (sorting) {
-  //   sorting = false;
-  //   for (let i = 0; i < q.length - 1; i++) {
-  //     if (q[i] > q[i + 1]) {
-  //       sorting = true;
-  //       if (!bribes[q[i]]) {
-  //         bribes[q[i]] = 1;
-  //       } else {
-  //         bribes[q[i]]++;
-  //       }
-
-  //       [q[i], q[i + 1]] = [q[i + 1], q[i]];
-  //     }
-  //   }
-  // }
-
-  // let total = 0;
-  // let isChaos = false;
-  // for (let [key, value] of Object.entries(bribes)) {
-  //   total+= value;
-  //   if (value > 2) {
-  //     isChaos = true;
-  //   }
-  // }
-
-  // if (isChaos) {
-  //   console.log('Too chaotic');
-  // } else {
-  //   console.log(total)
-  // }
+  if (isChaos) {
+    console.log('Too chaotic');
+  } else {
+    console.log(total)
+  }
 }
 
 minBribes([1, 2, 5, 3, 7, 8, 6, 4])
