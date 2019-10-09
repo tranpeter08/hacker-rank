d51a37b373f05c1bbd45d6150ac20039523bbc5d
Peter Tran <tran.peter08@gmail.com>
2019-10-08 18:16:36 -0700

    added no cases

--- ransom-note/ransome-note.test.js
+++ ransom-note/ransome-note.test.js
@@ -4,15 +4,28 @@ const testCasesYes = [
   ['Give win 1000 today'.split(' '), ['Give', '1000', 'today']],
   ['Need a loan one million'.split(' '), ['Need', 'one', 'million']],
   ['trouble money give else top trouble'.split(' '), ['trouble', 'give', 'money','trouble']]
-]
+];
+
+const testCasesNo = [
+  ['Give win 1000 today'.split(' '), ['give', '1000', 'today']],
+  ['Need a loan one million'.split(' '), ['Need', 'One', 'million']],
+  ['trouble money give else top'.split(' '), ['trouble', 'give', 'money','trouble']]
+];
 
 describe('ransomeNote()', () => {
+  let print;
+  console.log = jest.fn(message => print = message);
   it('prints "Yes" if words in notes matches words in magazine', () => {
-    let print;
-    console.log = jest.fn(message => print = message);
     testCasesYes.forEach((testCase,i) => {
       ransomeNote(...testCase);
       expect(print).toBe('Yes');
+    });
+  });
+
+  it('prints "No" if words in magazine do not match notes', () => {
+    testCasesNo.forEach((testCase,i) => {
+      ransomeNote(...testCase);
+      expect(print).toBe('No');
     });
   });
 });
